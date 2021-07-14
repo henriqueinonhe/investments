2b507048a05a6f0da1e99207fa7286d90b277350
Henrique Inonhe <henriqueinonhe@gmail.com>
2021-07-14 11:42:07 -0300

    Implement Swagger

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/health", (req, res) => {
+  res.send("Ok");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server up at ${process.env.PORT}!`);
+});
