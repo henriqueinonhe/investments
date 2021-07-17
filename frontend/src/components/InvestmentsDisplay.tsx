7f1fc0ca77dd94175bec188e5c41b5934c212985
Henrique Inonhe <henriqueinonhe@gmail.com>
2021-07-17 15:56:15 -0300

    More

---
+++ frontend/src/components/InvestmentsDisplay.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  overflow-y: scroll;
+`;
+
+export function InvestmentsDisplay() : JSX.Element {
+  return (
+    <Container>
+    </Container>
+  ) ;
+}
