9bf74c8cb5f2f951b8461e35390f23814ac3e961
Henrique Inonhe <henriqueinonhe@gmail.com>
2021-07-18 15:42:25 -0300

    Adapt error handling for express-jwt middleware

--- api/src/middlewares/handleError.ts
+++ api/src/middlewares/handleError.ts
@@ -1,12 +1,12 @@
 import { ValidationError } from "../exceptions/ValidationError.js";
 import { AuthenticationError } from "../exceptions/AuthenticationError.js";
 import { ResourceNotFoundError } from "../exceptions/ResourceNotFoundError.js";
-import express from "express";
+import { Request, Response, NextFunction, ErrorRequestHandler } from "express";
 
-export async function handleError(error : express.ErrorRequestHandler, 
-                                  req : express.Request, 
-                                  res : express.Response, 
-                                  next : express.NextFunction) : Promise<void> {
+export async function handleError(error : ErrorRequestHandler, 
+                                  req : Request, 
+                                  res : Response, 
+                                  next : NextFunction) : Promise<void> {
   console.log({
     req: {
       url: req.originalUrl,
@@ -22,6 +22,9 @@ export async function handleError(error : express.ErrorRequestHandler,
     res.status(422).send({ error });
   }
   else if(error instanceof AuthenticationError) {
+    res.status(401).send({ error });
+  }
+  else if(error.name === "UnauthorizedError") {
     res.status(401).send({ error });
   }
   else if(error instanceof ResourceNotFoundError) {
